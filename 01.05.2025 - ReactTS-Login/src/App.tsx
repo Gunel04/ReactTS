5e3204443642dcd86a079b125f77748f47d1f4fc
Gunel04 <alkhasovagunel@gmail.com>
2025-04-30 23:02:58 +0400

    list

--- 01.05.2025 - ReactTS-Login/src/App.tsx
+++ 01.05.2025 - ReactTS-Login/src/App.tsx
@@ -1,6 +1,20 @@
-const App = () => {
+type propsType = {
+  first:string,
+  second:string,
+  third:number
+}
+
+
+const App:React.FC<propsType> = ({first, second, third}) => {
   return (
-    <div>App</div>
+    <>
+      <div>App</div>
+      <ul>
+        <li>{first}</li>
+        <li>{second}</li>
+        <li>{third}</li>
+      </ul>
+    </>
   )
 }
 
--- 01.05.2025 - ReactTS-Login/src/main.tsx
+++ 01.05.2025 - ReactTS-Login/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import Login from './Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App first='Gunel' second='Alxasova' third={5} />
+    <Login />
   </StrictMode>
 )
