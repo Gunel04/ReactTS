bf8185626cc2e1a38a5ba34be0868ffae77f1dc3
Gunel04 <alkhasovagunel@gmail.com>
2025-05-01 14:17:53 +0400

    login-with-reactts

--- 01.05.2025 - ReactTS-Login/src/Login.tsx
+++ 01.05.2025 - ReactTS-Login/src/Login.tsx
@@ -23,57 +23,73 @@ console.log(myInfo);
 
 
 
+
+
 const Login: React.FC<inputTypes> = ({ input1, input2, input3, icon1, icon2, icon3 }) => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
-
+    const [check, setCheck] = useState<boolean>(false); 
+    console.log(check);
     const formSubmitted = (e) => {
         e.preventDefault();
-        if (!email || !password) {
-            Swal.fire({
-                icon: `${icon1}`,
-                text: "Please fill inputs!"
-            })
-        }
-        else if (email === myInfo.email && password === myInfo.password) {
-            Swal.fire({
-                icon: `${icon2}`,
-                text: "Login is completed successfully"
-            })
+        if (check) {
+            if (!email || !password) {
+                Swal.fire({
+                    icon: `${icon1}`,
+                    title: "Please fill inputs!"
+                })
+            }
+            else if (email === myInfo.email && password === myInfo.password) {
+                Swal.fire({
+                    icon: `${icon2}`,
+                    title: "Login is completed successfully"
+                })
+            }
+            else {
+                Swal.fire({
+                    icon: `${icon3}`,
+                    title: "Email address or password is wrong!"
+                })
+            }
         }
         else {
             Swal.fire({
-                icon: `${icon3}`,
-                text: "Email address or password is wrong!"
+                icon:`${icon1}`,
+                title:"Please accept the terms and policy!"
             })
         }
+        
     }
     return (
         <div className="entire-div">
             <div className="container-fluid">
                 <div className="row d-flex justify-content-center align-items-center">
 
-                    <h1 className="text-center my-4">Login</h1>
-                    <div className="col-3">
-                        <form>
-                            <div className="mb-3">
-                                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                <input type={input1} className="form-control" id="exampleInputEmail1" onChange={e => setEmail(e.target.value)} />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                <input type={input2} className="form-control" id="exampleInputPassword1" onChange={e => setPassword(e.target.value)} />
-                            </div>
-                            <div className="text-part d-flex justify-content-between">
-                                <div className="mb-3 form-check">
-                                    <input type={input3} className="form-check-input" id="exampleCheck1" />
-                                    <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
+                    <h1 className="text-center my-4 text-light">Login</h1>
+                    <div className="col-3 ">
+                        <div className="form-div">
+                            <form>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputEmail1" className="form-label text-light">Email address</label>
+                                    <input type={input1} className="form-control" id="exampleInputEmail1" onChange={e => setEmail(e.target.value)} />
                                 </div>
-                                <a href="#">Forgot password?</a>
-                            </div>
-                            <button className="btn btn-primary" style={{ width: "100%" }} onClick={formSubmitted}>Login</button>
-                            <p className="text-center my-2">Don't have an account? <a href="#">Register</a></p>
-                        </form>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputPassword1" className="form-label text-light">Password</label>
+                                    <input type={input2} className="form-control" id="exampleInputPassword1" onChange={e => setPassword(e.target.value)} />
+                                </div>
+                                <div className="text-part d-flex justify-content-between">
+                                    <div className="mb-3 form-check">
+                                        <input type={input3} className="form-check-input" id="exampleCheck1" onChange={e=>setCheck(e.target.checked)} />
+                                        <label className="form-check-label text-light" htmlFor="exampleCheck1">Remember me</label>
+                                    </div>
+                                    <a href="#">Forgot password?</a>
+                                    
+                                </div>
+                                <button className="btn btn-primary" style={{ width: "100%" }} onClick={formSubmitted}>Login</button>
+                                <p className="text-center my-2 text-light" >Don't have an account? <a href="#">Register</a></p>
+                            </form>
+                        </div>
+
 
                     </div>
                 </div>
