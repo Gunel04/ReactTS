f28780ce424d2173c7b8b6047a685fe90176c04e
Gunel04 <alkhasovagunel@gmail.com>
2025-05-10 09:51:18 +0400

    redux-toolkit-todo

--- 10.05.2025 - Redux-toolkit-Todo/src/App.tsx
+++ 10.05.2025 - Redux-toolkit-Todo/src/App.tsx
@@ -1,8 +1,8 @@
 import { useRef, type FormEvent } from "react"
-import { Button, Col, Container, Form, InputGroup, ListGroup, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux";
 import Swal from "sweetalert2";
-import { add } from "./features/todoSlice";
+import { add, deleteTodo } from "./features/todoSlice";
 
 const App: React.FC = () => {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -24,17 +24,17 @@ const App: React.FC = () => {
   }
   return (
     <Container>
-      <Row className="d-flex justify-content-center align-items-center my-5">
-        <Col sm={5}>
-          <h3 className="text-center">My todo App</h3>
-          <form className="input-group mb-3" onSubmit={submitForm}>
+      <h3 className="text-center mt-4 mb-5">My todo App</h3>
+      <Row className="d-flex justify-content-center align-items-center my-4">
+        <Col sm={4}>
+          <form className="input-group mb-5" onSubmit={submitForm}>
             <input ref={inputRef} type="text" className="form-control" placeholder="Add new Todo" />
             <button className="btn btn-outline-info" type="submit" id="button-addon2">Add</button>
           </form>
 
           <ul className="list-group">
-            {listItem.map((item: any, index:number) => (
-              <li key={index} className="list-group-item d-flex justify-content-between align-items-center">{item.text} <button className="btn btn-danger">X</button> </li>
+            {listItem.map((item: any, index: number) => (
+              <li key={index} className="list-group-item d-flex justify-content-between align-items-center">{index + 1}. {item.text} <button className="btn btn-danger" onClick={() => { dispatch(deleteTodo(item.id)) }}>X</button> </li>
 
             ))}
           </ul>
--- 10.05.2025 - Redux-toolkit-Todo/src/features/todoSlice.ts
+++ 10.05.2025 - Redux-toolkit-Todo/src/features/todoSlice.ts
@@ -14,13 +14,13 @@ const todoSlice = createSlice({
         add: (state, action: PayloadAction) => {
             const newTodo: todoType = { id: crypto.randomUUID(), text: action.payload };
             state.push(newTodo);
+        },
+        delete: (state, action)=> {
+            return state.filter(p=>p.id !== action.payload )
         }
-        // delete: (state, action)=> {
-
-        // }
     }
 })
 
-export const {add} = todoSlice.actions;
+export const {add, delete:deleteTodo} = todoSlice.actions;
 
 export default todoSlice.reducer;
